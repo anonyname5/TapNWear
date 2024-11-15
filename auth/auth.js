05c9fcddd48857911ffc25409b4ba1926e1f1d3e
AHMAD SYUKRI SAZALI <149860006+anonyname5@users.noreply.github.com>
2024-11-16 03:40:11 +0800

    Delete auth directory

--- auth/auth.js
+++
@@ -1,6 +0,0 @@
-function checkAdminAccess() {
-    const isAdmin = localStorage.getItem('isAdmin'); // Or however you store admin status
-    if (!isAdmin) {
-        window.location.href = '/login.html';
-    }
-} 
